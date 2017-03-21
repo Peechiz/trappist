86a5f68e3ffde314c5d90b16a9efbfcdc18b253a
Chris Impicciche <cimpicci@gmail.com>
2017-03-20 23:24:08 -0500

    fixes fade

--- sun.js
+++ sun.js
@@ -1,5 +1,5 @@
-function Sun(origin, r) {
-  this.pos = origin;
+function Sun(r) {
+  this.pos = createVector(0,0)
   this.hue = 0;
   this.r = map(r,0.11,1.13,5,25);
 }
@@ -9,12 +9,11 @@ Sun.prototype.show = function(){
   noStroke();
   ellipse(this.pos.x, this.pos.y, this.r * 2)
 
-  for (var i = 0; i < 30; i++) {
-    var myAlpha = map(i, 0, 30, 0, 255);
-    noStroke();
-    fill(this.hue,100,100,myAlpha)
-    //ellipse(this.pos.x, this.pos.y, this.r + i*2, this.r + i*2)
-  }
+  fill(this.hue, 100, 100, .5)
+  ellipse(this.pos.x, this.pos.y, this.r * 2 + 5)
 
-  this.hue = this.hue + 1 % 360
+  fill(this.hue, 100, 100, .2)
+  ellipse(this.pos.x, this.pos.y, this.r * 2 + 10)
+
+  this.hue = (this.hue + 1) % 360
 }
